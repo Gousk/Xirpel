2486e257fe3ac0b0ccd6107180c966d936b3e0a1
Önder Balta <onderman70@gmail.com>
2022-03-14 16:29:52 +0300

    14.03.2022

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,13 +9,22 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] GameObject deathVFX;
     [SerializeField] GameObject hitVFX;
-    [SerializeField] Transform parent;
 
+    GameObject parentGameObject;
     ScoreBoard scoreBoard;
 
     void Start() 
     {
-        scoreBoard = FindObjectOfType<ScoreBoard>();    
+        scoreBoard = FindObjectOfType<ScoreBoard>();  
+        AddRigidbody();
+
+        parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
+    }
+
+    void AddRigidbody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
     void OnParticleCollision(GameObject other)
@@ -31,7 +40,7 @@ public class Enemy : MonoBehaviour
     void ProcessHit()
     {
         GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         healthPoint--;
         scoreBoard.IncreaseScore(scorePerHit);
     }
@@ -39,7 +48,7 @@ public class Enemy : MonoBehaviour
     void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         Destroy(gameObject);
     }
 }
